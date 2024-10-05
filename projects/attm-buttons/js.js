65025e85696e1726a95b797e1f101a9fe365fb76
aarextiaokhiao <aarextiaokhiao@gmail.com>
2024-10-04 22:58:21 -0400

    ATTM Buttons: 2024 Edition

--- projects/attm-buttons/js.js
+++ projects/attm-buttons/js.js
@@ -1,44 +1,65 @@
 let BUTTONS = [
 	{
-		btn_text: "Far Past",
+		text: "Far Past",
 		btn_color: "#00e",
 
 		range: "2001 - 2011",
 		bg: "radial-gradient(ellipse at top, #fee, #fdd)"
 	}, {
-		btn_text: "Very Past",
+		text: "Very Past",
 		btn_color: "#07e",
 
 		range: "2014 - 2017",
 		bg: "radial-gradient(ellipse at top, #fee, #fdd)"
 	}, {
-		btn_text: "Past",
+		text: "Past",
 		btn_color: "#0ee",
 
 		range: "2017 - 2021",
 		bg: "radial-gradient(ellipse at top, #fee, #fdd)"
 	}, {
-		btn_text: "Present",
+		text: "Present",
 		btn_color: "#0e0",
 
 		start: true,
 		range: "2021 - 2024",
 	}, {
-		btn_text: "Future",
+		text: "Future",
 		btn_color: "#ee0",
 
 		range: "2025+",
 		bg: "radial-gradient(ellipse at top, #eef, #ddf)",
 		img: "../../images/icon.png"
+	}, {
+		text: "2024 Style I",
+		text_gradient: "linear-gradient(black, white)",
+
+		btn_color: "linear-gradient(#111, black)",
+		btn_border: "#111",
+
+		bg: "black",
+	}, {
+		text: "2024 Style II",
+		text_color: "#bbf",
+		text_stroke: "#33f",
+
+		btn_color: "linear-gradient(#33f, black)",
+		btn_border: "#33f",
+
+		range: "This is cool!",
+		bg: "linear-gradient(black, blue, white)",
 	}
 ]
 let BTN_CHOSEN = 0
 
 const DEFAULT = {
+	btn_color: "#fff",
+	btn_border: "black",
+	text: "???",
+	text_color: "black",
+
 	bg: "radial-gradient(ellipse at top, #fff, #def)",
-	range: "???",
-	btn_text: "???",
-	btn_color: "#fff"
+	range: "???"
 }
 
 //BUTTONS
@@ -62,7 +83,11 @@ function on_click(i) {
 function on_load() {
 	let btn_html = ``
 	for (var [i, btn] of Object.entries(BUTTONS)) {
-		btn_html += `<button id="btn_${i}" onclick="on_click(${i})" style="background: ${btn.btn_color ?? DEFAULT.btn_color}">${btn.btn_text ?? DEFAULT.btn_text}</button>`
+		let text = `${btn.text ?? DEFAULT.text}`, style = ""
+		if (btn.text_gradient) style += `background: ${btn.text_gradient}; -webkit-background-clip:text; -webkit-text-fill-color: transparent`
+		else if (btn.text_stroke) style += `text-shadow: ${btn.text_stroke} 1.5px 2px, ${btn.text_stroke} 1.5px -1.5px, ${btn.text_stroke} -1.5px 2px, ${btn.text_stroke} -1.5px -1.5px`
+
+		btn_html += `<button id="btn_${i}" onclick="on_click(${i})" style="border-color: ${btn.btn_border ?? DEFAULT.btn_border}; color: ${btn.text_color ?? DEFAULT.text_color}; background: ${btn.btn_color ?? DEFAULT.btn_color}"><div style='${style}'>${text}</div></button>`
 		if (btn.start) BTN_CHOSEN = i
 	}
 	el("btns").innerHTML = btn_html
